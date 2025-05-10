9a1fc051d5998c9a9e39b34b32bd914fb9292ebe
Theo <148655093+nzaantysadi@users.noreply.github.com>
2025-05-10 20:33:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-
-document.getElementById('flyBtn').addEventListener('click', () => {
-  const plane = document.getElementById('plane');
-  const letter = document.getElementById('letter');
-  plane.style.left = '100vw';
-  letter.style.left = 'calc(100vw - 200px)';
-});
+// Currently no extra JS
